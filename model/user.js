e689d5d7f7472bdaa53e6c52f83bf461a3e740d2
Anish Kumar Singh <anishsingh74850@gmail.com>
2024-05-17 20:25:19 +0530

    env config in mogodb

--- model/user.js
+++ model/user.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_CONNECT_URI || "mongodb+srv://esesocietyiitism:rER1nbNz3eR92Ztk@vibeshare.x5hcku3.mongodb.net/?retryWrites=true&w=majority&appName=VibeShare");
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_CONNECT_URI );
 // "mongodb://127.0.0.1:27017/socialMedia"
 // 
-// 
+// || "mongodb+srv://esesocietyiitism:rER1nbNz3eR92Ztk@vibeshare.x5hcku3.mongodb.net/?retryWrites=true&w=majority&appName=VibeShare"
 const userSchema = mongoose.Schema({
     username:String,
     name:String,
